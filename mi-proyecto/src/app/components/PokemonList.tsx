51f65ac654013a50abad4864b891f73b8679efe7
joacoesposito20 <espositojoaquin20@gmail.com>
2025-10-31 16:56:12 -0300

    Actividad6V2?

--- mi-proyecto/src/app/components/PokemonList.tsx
+++ mi-proyecto/src/app/components/PokemonList.tsx
@@ -1,27 +1,50 @@
 "use client";
 import PokemonItem from "./PokemonItem";
 import { useGetPokemonData } from "../hooks/UsePokemonAPI";
+import { useState } from "react";
 
 type Pokemon = {
   name: string,
   url: string;
 }
 
-export default function PokemonList(){ 
-  const {data:pokemones, isLoading, isError} = useGetPokemonData(30);
+const INITIAL_LIMIT = 20;
+const LOAD_MORE_AMOUNT = 10;
 
-  if (isLoading) return <p>Cargando pokemones...</p>;
+export default function PokemonList(){ 
+  const [limit, setLimit] = useState(INITIAL_LIMIT);
+  const {data:pokemons, isLoading, isError} = useGetPokemonData(limit);
+
+  const handleLoadMore = () =>{
+
+    setLimit(prevLimit => prevLimit + LOAD_MORE_AMOUNT)
+  }
+
   if (isError) return <p>Error al cargar pokemons</p>
   return(
     <div>
       <ul className="grid grid-cols-2 md:grid-cols-5 gap-2">
-      {pokemones && pokemones.map((item:Pokemon) => (
+      {pokemons && pokemons.map((item:Pokemon) => (
         <li key={item.name}>
           <PokemonItem name={item.name}></PokemonItem>
         </li>
       ))}
       </ul>
-    </div>  
+      
+      {/* **Botón "Cargar más"** */}
+      <div className="flex justify-center mt-6 mb-8">
+        <button
+          onClick={handleLoadMore}
+          // Deshabilitamos el botón mientras se está cargando para evitar clics múltiples
+          disabled={isLoading} 
+          className="px-6 py-3 bg-red-500 text-white font-bold rounded-lg shadow-md hover:bg-red-600 focus:outline-none focus:ring-4 focus:ring-red-300 disabled:bg-gray-400 transition"
+        >
+          Cargar más
+        </button>
+      </div>
+
+    </div>
+    
   );
 
 }
